e6d7324d320788188c6df9209ee0dcfe9dc0e7d5
ahmadf20 <afaaiz2@gmail.com>
2022-02-12 13:41:45 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by emir_ on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
